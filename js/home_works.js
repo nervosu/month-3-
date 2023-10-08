ccf68823a0e0fad2188c9656d56e08a78d8be6d5
nervosu <dzhamalov.alikhan@bk.ru>
2023-10-08 12:36:34 +0600

    first commit

--- js/home_works.js
+++ js/home_works.js
@@ -1,82 +1,28 @@
-// MOVE BLOCK
+const gmailButton = document.querySelector('#gmail_button')
+const gmailInput = document.querySelector('#gmail_input')
+const gmailResult = document.querySelector('#gmail_result')
+const regExp = /^\w{6,30}[@][g][m][a][i][l][.][c][o][m]/
+gmailButton.addEventListener('click', () => {
+    if (regExp.test(gmailInput.value)) {
+        gmailResult.innerHTML = 'OK'
+        gmailResult.style.color = 'green'
+    }else{
+        gmailResult.innerHTML = 'NOT OK'
+        gmailResult.style.color = 'red'
+    }
+})
 const childBlock = document.querySelector('.child_block')
-
-let positionX = 0
-let positionY = 0
-
-const move = () => {
-    if (positionX < 449 && positionY === 0) {
-        positionX+=2
-        childBlock.style.left = `${positionX}px`
-        setTimeout(move, 10)
-    } else if (positionX >= 449 && positionY < 449) {
-        positionY+=2
-        childBlock.style.top = `${positionY}px`
-        setTimeout(move, 10)
-    } else if (positionX > 0 && positionY > 0) {
-        positionX-=2
-        childBlock.style.left = `${positionX}px`
-        setTimeout(move, 10)
-    } else if (positionX === 0 && positionY > 0) {
-        positionY-=2
-        childBlock.style.top = `${positionY}px`
-        setTimeout(move, 10)
+const animation = () => {
+    let positionNew = 0
+    const positionEnd = 450
+    const speedAnimation = 2
+const animationStart = () => {
+    positionNew += speedAnimation
+    childBlock.style.left = positionNew + 'px'
+    if (positionNew < positionEnd) {
+        requestAnimationFrame(animationStart)
     }
 }
-
-move()
-
-// STOPWATCH
-const minutesBlock = document.querySelector('#minutes'),
-    secondsBlock = document.querySelector('#seconds'),
-    mlSecondsBlock = document.querySelector('#ml-seconds'),
-    startButton = document.querySelector('#start'),
-    stopButton = document.querySelector('#stop'),
-    resetButton = document.querySelector('#reset')
-
-let interval
-let minutes = 0
-let seconds = 0
-let mlSeconds = 0
-
-const startTimer = () => {
-    mlSeconds++
-    mlSeconds <= 99 && (mlSecondsBlock.innerHTML = mlSeconds)
-    mlSeconds === 100 && (mlSecondsBlock.innerHTML = '00')
-
-    mlSecondsBlock.innerHTML = `0${mlSeconds}`
-    mlSeconds > 9 && (mlSecondsBlock.innerHTML = mlSeconds)
-    if (mlSeconds > 99) {
-        seconds++
-        secondsBlock.innerHTML = `0${seconds}`
-        mlSeconds = 0
-    }
-    seconds > 9 && (secondsBlock.innerHTML = seconds)
-    if (seconds > 59) {
-        minutes++
-        minutesBlock.innerHTML = `0${minutes}`
-        seconds = 0
-        secondsBlock.innerHTML = `0${seconds}`
-    }
-    minutes > 9 && (minutesBlock.innerHTML = minutes)
+animationStart()
 }
-
-startButton.onclick = () => {
-    clearInterval(interval)
-    interval = setInterval(startTimer,  10)
-}
-
-stopButton.onclick = () => {
-    clearInterval(interval)
-}
-
-resetButton.onclick = () => {
-    clearInterval(interval)
-    minutes = 0
-    seconds = 0
-    mlSeconds = 0
-    minutesBlock.innerHTML = '00'
-    secondsBlock.innerHTML = '00'
-    mlSecondsBlock.innerHTML = '00'
-}
-
+animation()
