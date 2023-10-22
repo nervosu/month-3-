5a10ba81ececb2dc7c875ecf33272197fb2bd761
nervosu <dzhamalov.alikhan@bk.ru>
2023-10-22 20:33:01 +0600

    dz

--- js/lesson.js
+++ js/lesson.js
@@ -56,3 +56,52 @@ tabsParent.onclick = (event) => {
     }
 };
 setInterval(nextSlide, 3000);
+
+//converter
+// const somInput = document.querySelector('#som')
+// const usdInput = document.querySelector('#usd')
+// const eurInput = document.querySelector('#eur')
+// somInput.addEventListener('input', ()=>{
+//     const request = new XMLHttpRequest()
+//     request.open("GET","../data/converter.json")
+//     request.setRequestHeader("Content-type","application/json")
+//     request.send()
+//     request.onload = () => {
+//         const response = JSON.parse(request.response)
+//         usdInput.value = (somInput.value / response.usd).toFixed(2)
+//         eurInput.value = (somInput.value/ response.eur).toFixed(2)
+//     }
+// })
+// DRY - don't repeat yourself
+// KISS - Keep it simple, stupidconst somInput = document.querySelector('#som');
+const somInput = document.querySelector('#som');
+const usdInput = document.querySelector('#usd');
+const eurInput = document.querySelector('#eur');
+
+const exchangeRates = {
+    usd: 89.32,
+    eur: 94.72
+};
+
+function convertCurrency(input, fromCurrency, toCurrency1, toCurrency2) {
+    if (input.value === '') {
+        toCurrency1.value = '';
+        toCurrency2.value = '';
+    } else {
+        const inputValue = parseFloat(input.value);
+        toCurrency1.value = (inputValue / exchangeRates[fromCurrency]).toFixed(2);
+        toCurrency2.value = (inputValue / exchangeRates[fromCurrency]).toFixed(2);
+    }
+}
+
+somInput.addEventListener('input', () => {
+    convertCurrency(somInput, 'usd', usdInput, eurInput);
+});
+
+usdInput.addEventListener('input', () => {
+    convertCurrency(usdInput, 'eur', somInput, eurInput);
+});
+
+eurInput.addEventListener('input', () => {
+    convertCurrency(eurInput, 'usd', somInput, usdInput);
+});
